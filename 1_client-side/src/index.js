a5160a0058c0a6f613c9a7d6c570c68aaeaaea80
Domenika Grinkeviciute Skorulske <d.grinkeviciute@gmail.com>
2022-09-07 18:09:14 +0300

    added redux

--- 1_client-side/src/index.js
+++ 1_client-side/src/index.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { reducer, initialState } from "./redux";
+
+// const store = configureStore({ reducer, preloadedState: initialState });
+const store = configureStore({ reducer });
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
